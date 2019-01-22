ef101b6832b6736d6c39da7d0ea372987893938d
Jonah Lipsky <jonahlipsky@gmail.com>
2019-01-22 17:29:36 -0500

    Getting ready to push to heroku

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,9 @@
 class Api::SessionsController < ApplicationController
   def create
     username, password = params[:user][:username], params[:user][:password]
-    # debugger
     @user = User.find_by_credentials(username, password)
     if @user
       login(@user)
-      # debugger
       render 'api/users/show'
     else
       render ["Invalid credentials"], status: 401

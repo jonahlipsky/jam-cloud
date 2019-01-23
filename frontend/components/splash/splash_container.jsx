c36b89aa53a1861fb5c1fa36983eaded15456be1
Jonah Lipsky <jonahlipsky@gmail.com>
2019-01-22 20:24:55 -0500

    Working on modal forms

--- frontend/components/splash/splash_container.jsx
+++ frontend/components/splash/splash_container.jsx
@@ -3,17 +3,22 @@ import { Link } from 'react-router-dom';
 
 export default (props) => {
   return(
-    <div class="splash-image">
-      <div class="splash-modals">
+    <div className="splash-image">
+      <div className="splash-auth-btns">
         <button>Sign In</button>
         <button>Create Account</button>
         <button>For Creators</button>
       </div>
-      <div class='splash-welcome-text'>
+      <div className='splash-welcome-text'>
         <h1>Discover music on Jam-Cloud</h1>
         <h2>Upload your own tracks, comment on others, find songs, connect
           with artists, and stay up to date about their latest  releases!</h2>
         <Link to="/upload">Start Uploading Today</Link>
+      </div>
+      <div className='modal'>
+
+      </div>
+      <div className="modal-form">
       </div>
     </div>
   )

889f792ecd3cb7572cf841ba7877db6ee728ed6f
jonahlipsky <jonahlipsky@gmail.com>
2019-01-23 11:46:13 -0500

    Auth toggle working

---
+++ frontend/components/splash/modal_auth_form.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signin, signup } from '../../actions/session_actions';
+
+const mapDispatchToProps = dispatch => ({
+  signin: user => dispatch(signin(user)),
+  signup: user => dispatch(signup(user))
+});
+
+
+class ModalAuthForm extends React.Component {
+  constructor(props){
+    super(props);
+    // debugger
+    this.state = { email: '', password: ''};
+
+  }
+
+
+  handleSubmit(e){
+    e.preventDefault();
+    const user = {email: this.state.email, password: this.state.password};
+    if(this.state.formType === 'signin'){
+      this.props.signin(user);
+    } else {
+      this.props.signup(user);
+    }
+  }
+
+  handleChange(field){
+    return e => {
+      this.setState({[field]: e.target.value});
+    };
+  }
+
+  render(){
+    let submitButtonValue;
+    // debugger
+    if (this.props.formType === 'signin'){
+      submitButtonValue = 'Sign In';
+    } else {
+      submitButtonValue = 'Sign Up';
+    }
+    return(
+      <div className="modal-form">
+        <form className="options" onSubmit={this.handleSubmit}>
+          <input id='email' type="text" 
+            placeholder={'Please input your email address'} value={this.state.email}
+            onChange={this.handleChange('email')}/>
+          <input id='password' type="password" placeholder="Enter password" 
+            value={this.state.password} onChange={this.handleChange('password')}/>
+          <input id='submit' type="submit" value={submitButtonValue}/>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ModalAuthForm);
--- frontend/components/splash/splash_container.jsx
+++ frontend/components/splash/splash_container.jsx
@@ -1,43 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ModalAuthForm from './modal_auth_form';
 
 class Splash extends React.Component {
   constructor(props){
     super(props);
-    this.toggleModal = this.toggleModal.bind(this);
-    this.state = {modal: 'modal js-modal-open'};
+    this.state = {modal: 'modal js-modal-close', formType: ''};
   }
   
-  toggleModal(e){
-    this.setState({modal: 'modal js-modal-open'});
+  toggleModal({formType, action}){
+    return e => {
+      debugger
+      if(action === 'open'){
+        this.setState({modal: 'modal js-modal-open', formType});
+      } else {
+        this.setState({modal: 'modal js-modal-close', formType});
+      }
+    };
+    
   }
 
+
   render(){
+
+    const formTypeForAuth = this.state.formType;
+    debugger
     return(
       <div className="splash-image">
         <div className="splash-auth-btns">
-          <button onClick={this.toggleModal}>Sign In</button>
-          <button onClick={this.toggleModal}>Create Account</button>
+          <button onClick={this.toggleModal({formType: 'signin', action: 'open' })}>Sign In</button>
+          <button onClick={this.toggleModal({formType: 'signup', action: 'open' })}>Create Account</button>
           <button>For Creators</button>
         </div>
+
         <div className='splash-welcome-text'>
           <h1>Discover music on Jam-Cloud</h1>
           <h2>Upload your own tracks, comment on others, find songs, connect
             with artists, and stay up to date about their latest  releases!</h2>
           <Link to="/upload">Start Uploading Today</Link>
         </div>
+
         <div className={this.state.modal}>
   
-          <div className="modal-form">
-            <form className="options">
-
-              <input id='email' type="text" value={'Please input your email address'}/>
-              <input id='password' type="password" value={'Password'}/>
-              <input id='submit' type="submit"/>
-            </form>
-          </div>
+          <ModalAuthForm formType={formTypeForAuth}/>
           
           <div className="modal-screen">
+            <button className="modal-close" 
+              onClick={this.toggleModal({ formType: '', action: 'close' })}>
+              X
+            </button>
           </div>
   
         </div>

8ce2065c3b967a15e63d7b701190c70b35467d10
jonahlipsky <jonahlipsky@gmail.com>
2019-01-31 21:42:11 -0500

    Finished Splash Page

--- frontend/components/reuseable_components/single_track_display.jsx
+++ frontend/components/reuseable_components/single_track_display.jsx
@@ -1,31 +1,71 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { pushToFrontOfQueue } from '../../actions/sound_controller_actions';
 
-export default (props) => {
 
-  // REFACTOR THIS IN TERMS OF A TUPLE TRACK ARTIST PAIR
-  // E.G. [trackOBJECT, artistObject]
+const mapDispatchToProps = dispatch => ({
+  pushToFrontOfQueue: (trackId) => dispatch(pushToFrontOfQueue(trackId))
+});
 
-  let track = props.trackArtistPair[0];
-  let artist = props.trackArtistPair[1];
+class SingleTrackDisplay extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {mouseOver: false, mouseOverPlayButton: false};
+    this.mouseEnter = this.mouseEnter.bind(this);
+    this.mouseEnterPlayButton = this.mouseEnterPlayButton.bind(this);
+    this.mouseLeave = this.mouseLeave.bind(this);
+    this.mouseLeavePlayButton = this.mouseLeavePlayButton.bind(this);
+    this.playTrack = this.playTrack.bind(this);
+  }
 
-  // let track = props.track;
-  // let artist = props.artist;
-  
-  let imageUrl = track ? track.imageUrl : '';
-  let title = track ? track.title : '';
-  let artistName = artist ? artist.username : '';
-  let trackId = track ? track.id : 1;
-  let userId = artist ? artist.id : 1;
+  mouseEnter(){
+    this.setState({mouseOver: true});
+  }
 
-  return(
-    <div className="single-track-display">
-      <img src={imageUrl} />
-      <div className="playButton"></div>
-      <div className="single-track-artist-title">
-        <Link className="title" to={`/tracks/${trackId}`}>{title}</Link>
-        <Link className="artist-name" to={`/users/${userId}`}>{artistName}</Link>
+  mouseLeave(){
+    this.setState({mouseOver: false});
+  }
+
+  mouseEnterPlayButton(){
+    this.setState({mouseOverPlayButton: true});
+  }
+
+  mouseLeavePlayButton(){
+    this.setState({mouseOverPlayButton: false});
+  }
+
+  playTrack(){
+    this.props.pushToFrontOfQueue(this.props.trackArtistPair[0].id);
+  }
+
+  render(){
+    let track = this.props.trackArtistPair[0];
+    let artist = this.props.trackArtistPair[1];
+
+    let imageUrl = track ? track.imageUrl : '';
+    let title = track ? track.title : '';
+    let artistName = artist ? artist.username : '';
+    let trackId = track ? track.id : 1;
+    let userId = artist ? artist.id : 1;
+    let playButtonClass = this.state.mouseOver ? "play-button" : "play-button hidden";
+    let playIcon = this.state.mouseOverPlayButton ? 
+      <img src={window.playIconHover} onClick={this.playTrack}/> : 
+      <img src={window.playIcon}/>
+
+    return(
+      <div className="single-track-display" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} >
+        <img className="track-main-image" src={imageUrl} />
+        <div className={playButtonClass} onMouseEnter={this.mouseEnterPlayButton} onMouseLeave={this.mouseLeavePlayButton}>
+          {playIcon}
+        </div>
+        <div className="single-track-artist-title">
+          <Link className="title" to={`/tracks/${trackId}`}>{title}</Link>
+          <Link className="artist-name" to={`/users/${userId}`}>{artistName}</Link>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default connect(null, mapDispatchToProps)(SingleTrackDisplay);

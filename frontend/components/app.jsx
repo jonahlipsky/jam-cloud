42d40eac09c101e18a500bb9297edf13f8150983
jonahlipsky <jonahlipsky@gmail.com>
2019-03-04 13:25:08 -0500

    Began media queries

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,18 +1,23 @@
 import SplashContainer from './splash/splash_container';
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Route, Redirect, NavLink, Switch, Link, HashRouter} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import MediaQuery from 'react-responsive';
 import Main from './main/main';
 import ContinuousPlayBar from './main/continuous_play_bar/play_bar';
 
 const App = (props) => (
   <div id="app">
-    
-    <Switch>
-      <AuthRoute exact path='/' component={SplashContainer} /> 
-      <ProtectedRoute path='/' component={Main} />
-    </Switch>
-    <ContinuousPlayBar/>
+    <MediaQuery query="(max-device-width: 1224px)">
+      <div>You are a tablet or mobile phone</div>
+    </MediaQuery>
+    <MediaQuery query="(min-device-width: 1225px)">  
+      <Switch>
+        <AuthRoute exact path='/' component={SplashContainer} /> 
+        <ProtectedRoute path='/' component={Main} />
+      </Switch>
+      <ContinuousPlayBar/>
+    </MediaQuery>
   </div>
 )
 
